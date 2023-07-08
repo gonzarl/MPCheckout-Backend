30c2f29d03ca749d8e2b82a479b70f90d7028f5b
gonzariquelme6 <gonzariquelme66@gmail.com>
2023-07-08 17:17:57 -0300

    File upload

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mercadopago = require("mercadopago");
+require("dotenv").config()
+
+mercadopago.configure({
+    access_token: process.env.MERCADO_PAGO_ACCESS_TOKEN,
+});
+
+
+app.use(express.json());
+app.use(cors());
+app.get("/", function (req, res) {
+    res.send("Server is running :)");
+});
+
+app.post("/create_preference", (req, res) => {
+
+    let preference = {
+        items: [
+            {
+                title: req.body.description,
+                unit_price: Number(req.body.price),
+                quantity: Number(req.body.quantity),
+            }
+        ],
+        //a que paginas redirigir si hay exito o falla
+        back_urls: {
+            "success": "http://localhost:5173",
+            "failure": "http://localhost:5173",
+            "pending": ""
+        },
+        auto_return: "approved",
+    };
+
+    mercadopago.preferences.create(preference)
+        .then(function (response) {
+            res.json({
+                id: response.body.id
+            });
+        }).catch(function (error) {
+            console.log(error);
+        });
+});
+
+app.listen(8080, () => {
+    console.log("The server is now running on Port 8080");
+});
